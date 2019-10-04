da3c5ff63e4f4e77ddf5b4828c71521df6f1e981
febtrianiari <febtria.niari@gmail.com>
2019-10-04 15:22:31 +0700

    UTS Project 1

---
+++ app/src/main/java/id/ac/polinema/utsprojectfebtrianp/halamanMenuUtama.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.utsprojectfebtrianp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class halamanMenuUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_menu_utama);
+    }
+}
