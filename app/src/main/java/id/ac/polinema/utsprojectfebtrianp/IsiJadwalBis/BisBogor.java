ef84635b44cd60303bcceb0c5455e4d7dea4957e
febtrianiari <febtria.niari@gmail.com>
2019-10-07 21:10:53 +0700

    Uts Project 4

---
+++ app/src/main/java/id/ac/polinema/utsprojectfebtrianp/IsiJadwalBis/BisBogor.java
@@ -0,0 +1,32 @@
+package id.ac.polinema.utsprojectfebtrianp.IsiJadwalBis;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.ac.polinema.utsprojectfebtrianp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BisBogor extends Fragment {
+
+
+    public BisBogor() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bis_bogor, container, false);
+    }
+
+}
